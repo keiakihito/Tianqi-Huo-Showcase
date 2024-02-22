6160ef6d2a38e3eea9a9320478ebb794a794cd36
keiakihito <kkatsumi.git@gmail.com>
2024-02-21 16:56:48 -0800

    Fix position task 3 and video

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <p>Hello World! These are Tianqi Huo's sample work</p>
-          </header>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello World! These are Tianqi Huo's sample work</p>
+            </header>
 
-          {/*project 1*/}
-          <p>Clickable Prototype Task 1</p>
-          <iframe
-              width="375"
-              height="812"
-              src="https://xd.adobe.com/embed/b5c53ab3-14e2-4866-81f4-ff095190eaaf-e096/"
-              frameBorder="0"
-              allowFullScreen
-              title="Project 1 Description">
-          </iframe>
+            {/*project 1*/}
+            <div className="project">
+                <p>Clickable Prototype Task 1</p>
+                <iframe
+                    width="375"
+                    height="812"
+                    src="https://xd.adobe.com/embed/b5c53ab3-14e2-4866-81f4-ff095190eaaf-e096/"
+                    frameBorder="0"
+                    allowFullScreen
+                    title="Project 1 Description">
+                </iframe>
+            </div>
 
-          {/*project2*/}
-          <p>Clickable Prototype Task 2</p>
-          <iframe
-              width="375"
-              height="812"
-              src="https://xd.adobe.com/embed/e3363f98-3603-4607-97ea-aa0b5151aad2-b665/"
-              frameBorder="0"
-              allowFullScreen
-              title="Project 2 Description">
-          </iframe>
+            {/*project2*/}
+            <div className="project">
+                <p>Clickable Prototype Task 2</p>
+                <iframe
+                    width="375"
+                    height="812"
+                    src="https://xd.adobe.com/embed/e3363f98-3603-4607-97ea-aa0b5151aad2-b665/"
+                    frameBorder="0"
+                    allowFullScreen
+                    title="Project 2 Description">
+                </iframe>
+            </div>
 
-          {/*project3*/}
-          <p>Clickable Prototype Task 3</p>
-          <iframe
-              width="375"
-              height="812"
-              src="https://xd.adobe.com/embed/53128845-27b7-4e95-be24-31c78a3c4e1e-0641/"
-              frameBorder="0"
-              allowFullScreen
-              title="Project 3 Description"> 
-          </iframe>
+            {/*project3*/}
+            <div className="project">
+                <p>Clickable Prototype Task 3</p>
+                <iframe
+                    width="375"
+                    height="812"
+                    src="https://xd.adobe.com/embed/53128845-27b7-4e95-be24-31c78a3c4e1e-0641/"
+                    frameBorder="0"
+                    allowFullScreen
+                    title="Project 3 Description">
+                </iframe>
+            </div>
 
-
-          <video width="320" height="240" controls>
-              <source src="AsMod4.2_TianqiHuo.mp4" type="video/mp4"/>
-              Your browser does not support the video tag.
-          </video>
-      </div>
-  );
+            {/*video*/}
+            <div className="video">
+                <video width="320" height="240" controls>
+                    <source src="AsMod4.2_TianqiHuo.mp4" type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    );
 }
 
 export default App;
