e9365fe7939925efca2702c2624cd2cab819d7bc
keiakihito <kkatsumi.git@gmail.com>
2024-02-21 16:47:39 -0800

    Debug iframe with unique titles

--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ function App() {
           <header className="App-header">
               <p>Hello World! These are Tianqi Huo's sample work</p>
           </header>
+
           {/*project 1*/}
           <p>Clickable Prototype Task 1</p>
-          <iframe width="375" height="812" src="https://xd.adobe.com/embed/b5c53ab3-14e2-4866-81f4-ff095190eaaf-e096/"
-                  frameBorder="0" allowFullScreen></iframe>
+          <iframe
+              width="375"
+              height="812"
+              src="https://xd.adobe.com/embed/b5c53ab3-14e2-4866-81f4-ff095190eaaf-e096/"
+              frameBorder="0"
+              allowFullScreen
+              title="Project 1 Description"> // Add a descriptive title here
+          </iframe>
 
           {/*project2*/}
           <p>Clickable Prototype Task 2</p>
-          <iframe width="375" height="812" src="https://xd.adobe.com/embed/e3363f98-3603-4607-97ea-aa0b5151aad2-b665/"
-                  frameBorder="0" allowFullScreen></iframe>
+          <iframe
+              width="375"
+              height="812"
+              src="https://xd.adobe.com/embed/e3363f98-3603-4607-97ea-aa0b5151aad2-b665/"
+              frameBorder="0"
+              allowFullScreen
+              title="Project 2 Description"> // And here
+          </iframe>
 
           {/*project3*/}
           <p>Clickable Prototype Task 3</p>
-          <iframe width="375" height="812" src="https://xd.adobe.com/embed/53128845-27b7-4e95-be24-31c78a3c4e1e-0641/"
-                  frameBorder="0" allowFullScreen></iframe>
+          <iframe
+              width="375"
+              height="812"
+              src="https://xd.adobe.com/embed/53128845-27b7-4e95-be24-31c78a3c4e1e-0641/"
+              frameBorder="0"
+              allowFullScreen
+              title="Project 3 Description"> // And also here
+          </iframe>
 
 
           <video width="320" height="240" controls>
