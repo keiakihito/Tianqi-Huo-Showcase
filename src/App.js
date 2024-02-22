43ae492124d2663b91f0872a8ea9225b9cfdaa1f
keiakihito <kkatsumi.git@gmail.com>
2024-02-21 16:43:32 -0800

    Adding clickable temporary prototypes and video sample

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-          <p>Hello World! Here will be a Tianqi Huo showcase page</p>
-        </header>
-      </div>
+          <header className="App-header">
+              <p>Hello World! These are Tianqi Huo's sample work</p>
+          </header>
+          {/*project 1*/}
+          <p>Clickable Prototype Task 1</p>
+          <iframe width="375" height="812" src="https://xd.adobe.com/embed/b5c53ab3-14e2-4866-81f4-ff095190eaaf-e096/"
+                  frameBorder="0" allowFullScreen></iframe>
 
+          {/*project2*/}
+          <p>Clickable Prototype Task 2</p>
+          <iframe width="375" height="812" src="https://xd.adobe.com/embed/e3363f98-3603-4607-97ea-aa0b5151aad2-b665/"
+                  frameBorder="0" allowFullScreen></iframe>
+
+          {/*project3*/}
+          <p>Clickable Prototype Task 3</p>
+          <iframe width="375" height="812" src="https://xd.adobe.com/embed/53128845-27b7-4e95-be24-31c78a3c4e1e-0641/"
+                  frameBorder="0" allowFullScreen></iframe>
+
+
+          <video width="320" height="240" controls>
+              <source src="AsMod4.2_TianqiHuo.mp4" type="video/mp4"/>
+              Your browser does not support the video tag.
+          </video>
+      </div>
   );
 }
 
